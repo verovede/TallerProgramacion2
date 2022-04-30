47f7b164b11c985d21a1cea8ac5a8294f952a478
verovede <veroferrariback@gmail.com>
2022-04-30 13:23:02 -0300

    30/04/22

--- demos/async/fileReadAsync.js
+++ demos/async/fileReadAsync.js
@@ -1,15 +1,17 @@
+//let inventors = JSON.parse(fs.readFileSync(PATH, 'utf-8')); --> el sync es para sacarlo del default que asincrono
+
 // fyle system es el que permite trabajar con el sistema de archivos
+// const fs = require('fs'); -> esto puede decrepar
+// para usar el import hay que agregar al package.json -> "type": "module"
 
 import fs from 'fs';
 
-let datos = '';
-
+//fs.readFile -> es para leer el archivo (ubicacion del archivo , encoding para que lo lea como texto , callback (err,data) )
 fs.readFile('./bigfile.txt', 'utf-8', (err, data) => {
+    // si no hay error, nulo o vacio, devuelve la info de bigfile
     if (!err) {
-        console.log("Se ejecuto OK");
-        datos = data;
-        console.log(datos);
+        console.log("Se ejecuto OK");       
+        console.log(data);
     }
 })
 
-console.log("datos", datos);
--- demos/async/inventorsAsync.js
+++ demos/async/inventorsAsync.js
@@ -1,29 +1,40 @@
 // UTLIZANDO FUNCIONES ASINCRONAS DE LECTURA Y ESCRITURA
 
+// importar sistema de archivos
 import fs from 'fs';
 
+// levanto la informacion del json
 const path = './inventors.json'
 
+// creo el nuevo inventor
 const nuevoInventor = { first: 'Nuevo', last: 'Inventor', year: 2022 }
 
-//1. Leer el archivo inventors .json
+
+//1 Leer el archivo inventors .json
 fs.readFile(path, 'utf-8', (err, data) => {
     if (!err) {
-        //2. agregar inventor 
-        const inventors = JSON.parse(data)
+        console.log(data)
+
+        //2 Agregar el inventor
+        // me traigo la lista de inventores a una constante inventors    
+        let inventors = JSON.parse(data);
         inventors.push(nuevoInventor)
-        //3. escribir en el json incluyendo el nuevo inventor
-        fs.writeFileSync(
-            path, JSON.stringify(inventors, null, ' '), err => {
+
+        //3 esribir el json (persistir)
+        fs.writeFile(path, JSON.stringify(inventors, null, ' '), err => {
             if (!err) {
-                //4. leer el archivo con el nuevo inventor
-                fs.readFile(path, 'utf-8', (err, data) => {
+                console.log("Se escribio sin errores")
+
+                //4 leer el archivo con el cambio
+                fs.readFile(path, 'utf8', (err, data) => {
                     if (!err) {
-                        console.log(JSON.parse(data));
-                    }    
+                        console.log("Listado con cambio")
+                        console.log(data)
+                    }
                 })
-                // fin punto 4
             }
-        })// fin punto 3
-    }
+        })
+
+    } else { console.log("No se encontro el archivo") }
+
 })
